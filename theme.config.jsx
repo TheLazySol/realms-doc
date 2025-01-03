8d0f9afbd2a5b5a800977f26f2572e74297c4730
Utkarsh <83659045+0xShuk@users.noreply.github.com>
2025-01-03 19:02:32 +0530

    update twitter link

--- theme.config.jsx
+++ theme.config.jsx
@@ -21,7 +21,7 @@ export default {
   navbar: {
     extraContent: (
       <>
-        <a href="https://twitter.com/shuding_" target="_blank" rel="noopener noreferrer">
+        <a href="https://twitter.com/RealmsDAOs" target="_blank" rel="noopener noreferrer">
           <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path d="M23.954 4.569c-.885.389-1.83.654-2.825.775a4.958 4.958 0 002.163-2.723 9.92 9.92 0 01-3.127 1.184 4.916 4.916 0 00-8.384 4.482 13.934 13.934 0 01-10.112-5.14 4.904 4.904 0 001.523 6.548 4.876 4.876 0 01-2.228-.616c-.054 2.162 1.515 4.188 3.763 4.64a4.905 4.905 0 01-2.212.083 4.928 4.928 0 004.604 3.417 9.872 9.872 0 01-6.102 2.104c-.395 0-.779-.023-1.17-.067a13.945 13.945 0 007.548 2.213c9.142 0 14.307-7.721 13.995-14.646a9.935 9.935 0 002.457-2.548z"/>
           </svg>
